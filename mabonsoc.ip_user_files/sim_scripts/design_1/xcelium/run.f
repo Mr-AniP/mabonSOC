bd168e7125624ffd54e87f5d636061f1a1271b39
Animesh Pareek <108825332+Mr-AniP@users.noreply.github.com>
2023-10-26 20:22:21 +0000

    commit 4

---
+++ mabonsoc.ip_user_files/sim_scripts/design_1/xcelium/run.f
@@ -0,0 +1,83 @@
+-makelib xcelium_lib/xilinx_vip -sv \
+  "E:/xilinx/Vivado/2019.1/data/xilinx_vip/hdl/axi4stream_vip_axi4streampc.sv" \
+  "E:/xilinx/Vivado/2019.1/data/xilinx_vip/hdl/axi_vip_axi4pc.sv" \
+  "E:/xilinx/Vivado/2019.1/data/xilinx_vip/hdl/xil_common_vip_pkg.sv" \
+  "E:/xilinx/Vivado/2019.1/data/xilinx_vip/hdl/axi4stream_vip_pkg.sv" \
+  "E:/xilinx/Vivado/2019.1/data/xilinx_vip/hdl/axi_vip_pkg.sv" \
+  "E:/xilinx/Vivado/2019.1/data/xilinx_vip/hdl/axi4stream_vip_if.sv" \
+  "E:/xilinx/Vivado/2019.1/data/xilinx_vip/hdl/axi_vip_if.sv" \
+  "E:/xilinx/Vivado/2019.1/data/xilinx_vip/hdl/clk_vip_if.sv" \
+  "E:/xilinx/Vivado/2019.1/data/xilinx_vip/hdl/rst_vip_if.sv" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib -sv \
+  "E:/xilinx/Vivado/2019.1/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
+  "E:/xilinx/Vivado/2019.1/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv" \
+-endlib
+-makelib xcelium_lib/xpm \
+  "E:/xilinx/Vivado/2019.1/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib xcelium_lib/axi_infrastructure_v1_1_0 \
+  "../../../../mabonsoc.srcs/sources_1/bd/design_1/ipshared/ec67/hdl/axi_infrastructure_v1_1_vl_rfs.v" \
+-endlib
+-makelib xcelium_lib/axi_vip_v1_1_5 -sv \
+  "../../../../mabonsoc.srcs/sources_1/bd/design_1/ipshared/d4a8/hdl/axi_vip_v1_1_vl_rfs.sv" \
+-endlib
+-makelib xcelium_lib/processing_system7_vip_v1_0_7 -sv \
+  "../../../../mabonsoc.srcs/sources_1/bd/design_1/ipshared/8c62/hdl/processing_system7_vip_v1_0_vl_rfs.sv" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../bd/design_1/ip/design_1_processing_system7_0_0/sim/design_1_processing_system7_0_0.v" \
+-endlib
+-makelib xcelium_lib/lib_cdc_v1_0_2 \
+  "../../../../mabonsoc.srcs/sources_1/bd/design_1/ipshared/ef1e/hdl/lib_cdc_v1_0_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/proc_sys_reset_v5_0_13 \
+  "../../../../mabonsoc.srcs/sources_1/bd/design_1/ipshared/8842/hdl/proc_sys_reset_v5_0_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../bd/design_1/ip/design_1_rst_ps7_0_100M_2/sim/design_1_rst_ps7_0_100M_2.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../mabonsoc.srcs/sources_1/bd/design_1/ipshared/3266/hdl/verilog/log_28_15_s.v" \
+  "../../../../mabonsoc.srcs/sources_1/bd/design_1/ipshared/3266/hdl/verilog/log_28_15_s_log_abkb.v" \
+  "../../../../mabonsoc.srcs/sources_1/bd/design_1/ipshared/3266/hdl/verilog/log_28_15_s_log_acud.v" \
+  "../../../../mabonsoc.srcs/sources_1/bd/design_1/ipshared/3266/hdl/verilog/log_28_15_s_log_adEe.v" \
+  "../../../../mabonsoc.srcs/sources_1/bd/design_1/ipshared/3266/hdl/verilog/mabonsoc_BUS_A_s_axi.v" \
+  "../../../../mabonsoc.srcs/sources_1/bd/design_1/ipshared/3266/hdl/verilog/mabonsoc_mac_mulseOg.v" \
+  "../../../../mabonsoc.srcs/sources_1/bd/design_1/ipshared/3266/hdl/verilog/mabonsoc_mux_42_1fYi.v" \
+  "../../../../mabonsoc.srcs/sources_1/bd/design_1/ipshared/3266/hdl/verilog/mabonsoc_mux_42_1ibs.v" \
+  "../../../../mabonsoc.srcs/sources_1/bd/design_1/ipshared/3266/hdl/verilog/mabonsoc_sdiv_32ng8j.v" \
+  "../../../../mabonsoc.srcs/sources_1/bd/design_1/ipshared/3266/hdl/verilog/mabonsoc_udiv_18nhbi.v" \
+  "../../../../mabonsoc.srcs/sources_1/bd/design_1/ipshared/3266/hdl/verilog/sqrt_fixed_28_15_s.v" \
+  "../../../../mabonsoc.srcs/sources_1/bd/design_1/ipshared/3266/hdl/verilog/mabonsoc.v" \
+  "../../../bd/design_1/ip/design_1_mabonsoc_0_5/sim/design_1_mabonsoc_0_5.v" \
+-endlib
+-makelib xcelium_lib/generic_baseblocks_v2_1_0 \
+  "../../../../mabonsoc.srcs/sources_1/bd/design_1/ipshared/b752/hdl/generic_baseblocks_v2_1_vl_rfs.v" \
+-endlib
+-makelib xcelium_lib/fifo_generator_v13_2_4 \
+  "../../../../mabonsoc.srcs/sources_1/bd/design_1/ipshared/1f5a/simulation/fifo_generator_vlog_beh.v" \
+-endlib
+-makelib xcelium_lib/fifo_generator_v13_2_4 \
+  "../../../../mabonsoc.srcs/sources_1/bd/design_1/ipshared/1f5a/hdl/fifo_generator_v13_2_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/fifo_generator_v13_2_4 \
+  "../../../../mabonsoc.srcs/sources_1/bd/design_1/ipshared/1f5a/hdl/fifo_generator_v13_2_rfs.v" \
+-endlib
+-makelib xcelium_lib/axi_data_fifo_v2_1_18 \
+  "../../../../mabonsoc.srcs/sources_1/bd/design_1/ipshared/5b9c/hdl/axi_data_fifo_v2_1_vl_rfs.v" \
+-endlib
+-makelib xcelium_lib/axi_register_slice_v2_1_19 \
+  "../../../../mabonsoc.srcs/sources_1/bd/design_1/ipshared/4d88/hdl/axi_register_slice_v2_1_vl_rfs.v" \
+-endlib
+-makelib xcelium_lib/axi_protocol_converter_v2_1_19 \
+  "../../../../mabonsoc.srcs/sources_1/bd/design_1/ipshared/c83a/hdl/axi_protocol_converter_v2_1_vl_rfs.v" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../bd/design_1/ip/design_1_auto_pc_0/sim/design_1_auto_pc_0.v" \
+  "../../../bd/design_1/sim/design_1.v" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
---
+++ mabonsoc.srcs/sources_1/bd/design_1/ip/design_1_mabonsoc_0_5/design_1_mabonsoc_0_5_stub.vhdl
@@ -0,0 +1,49 @@
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
+-- Date        : Thu Aug 17 14:05:02 2023
+-- Host        : SHANNON-PC1 running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               C:/Users/animesh21131/mabonSOC/mabonsoc.srcs/sources_1/bd/design_1/ip/design_1_mabonsoc_0_5/design_1_mabonsoc_0_5_stub.vhdl
+-- Design      : design_1_mabonsoc_0_5
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg484-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity design_1_mabonsoc_0_5 is
+  Port ( 
+    s_axi_BUS_A_AWADDR : in STD_LOGIC_VECTOR ( 4 downto 0 );
+    s_axi_BUS_A_AWVALID : in STD_LOGIC;
+    s_axi_BUS_A_AWREADY : out STD_LOGIC;
+    s_axi_BUS_A_WDATA : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    s_axi_BUS_A_WSTRB : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    s_axi_BUS_A_WVALID : in STD_LOGIC;
+    s_axi_BUS_A_WREADY : out STD_LOGIC;
+    s_axi_BUS_A_BRESP : out STD_LOGIC_VECTOR ( 1 downto 0 );
+    s_axi_BUS_A_BVALID : out STD_LOGIC;
+    s_axi_BUS_A_BREADY : in STD_LOGIC;
+    s_axi_BUS_A_ARADDR : in STD_LOGIC_VECTOR ( 4 downto 0 );
+    s_axi_BUS_A_ARVALID : in STD_LOGIC;
+    s_axi_BUS_A_ARREADY : out STD_LOGIC;
+    s_axi_BUS_A_RDATA : out STD_LOGIC_VECTOR ( 31 downto 0 );
+    s_axi_BUS_A_RRESP : out STD_LOGIC_VECTOR ( 1 downto 0 );
+    s_axi_BUS_A_RVALID : out STD_LOGIC;
+    s_axi_BUS_A_RREADY : in STD_LOGIC;
+    ap_clk : in STD_LOGIC;
+    ap_rst_n : in STD_LOGIC;
+    interrupt : out STD_LOGIC
+  );
+
+end design_1_mabonsoc_0_5;
+
+architecture stub of design_1_mabonsoc_0_5 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "s_axi_BUS_A_AWADDR[4:0],s_axi_BUS_A_AWVALID,s_axi_BUS_A_AWREADY,s_axi_BUS_A_WDATA[31:0],s_axi_BUS_A_WSTRB[3:0],s_axi_BUS_A_WVALID,s_axi_BUS_A_WREADY,s_axi_BUS_A_BRESP[1:0],s_axi_BUS_A_BVALID,s_axi_BUS_A_BREADY,s_axi_BUS_A_ARADDR[4:0],s_axi_BUS_A_ARVALID,s_axi_BUS_A_ARREADY,s_axi_BUS_A_RDATA[31:0],s_axi_BUS_A_RRESP[1:0],s_axi_BUS_A_RVALID,s_axi_BUS_A_RREADY,ap_clk,ap_rst_n,interrupt";
+attribute X_CORE_INFO : string;
+attribute X_CORE_INFO of stub : architecture is "mabonsoc,Vivado 2019.1";
+begin
+end;
